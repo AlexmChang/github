24a2b31e10142946f603680e285a57c4ca776e2c
AlexmChang <alexmxchang@gmail.com>
2026-07-06 18:59:18 -0700

    optimizing

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 #ask user for their name
-name = input("what is your name? ")
+name = input("what is your name? ").strip().title()
 
-#remove whitespace from str
-name = name.strip()
+#remove whitespace from str and capitalize user name
 
 # Say hello to user
 print(f"hello, {name}")
